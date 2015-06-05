b9c5a224753c5b4ff83ccd332b736bd8fc3be4e1
Pedro Medeiros <pedrosnk@gmail.com>
2015-06-05 19:31:38 -0300

    plot 3 logic

---
+++ plot3.R
@@ -0,0 +1,17 @@
+
+source('getadata.R')
+
+data <- read_power_consumption()
+time_period <- strptime(paste(data$Date, data$Time), format='%d/%m/%Y %H:%M:%S')
+
+png(filename = 'plot3.png' , width = 480, height = 480)
+
+plot(time_period, data$Sub_metering_1, type='l')
+lines(time_period, data$Sub_metering_1, col='black')
+lines(time_period, data$Sub_metering_2, col='red')
+lines(time_period, data$Sub_metering_3, col='blue')
+
+legend('topright', legend=c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'),
+       col=c('black', 'red', 'blue'), lty=1)
+
+dev.off()
