2ccb3532771e3677e1e8ea39607d95178d4ef0f8
Pedro Medeiros <pedrosnk@gmail.com>
2015-06-05 20:01:51 -0300

    add some details missing in plot 1

--- plot1.R
+++ plot1.R
@@ -5,6 +5,7 @@ data <- read_power_consumption()
 
 png(filename = 'plot1.png' , width = 480, height = 480)
 
-hist(data$Global_active_power)
+hist(data$Global_active_power, main='Global Active Power',
+     xlab='Global Active Power (kilowatts)', col='red')
 
 dev.off()
